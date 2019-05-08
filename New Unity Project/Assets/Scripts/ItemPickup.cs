0b1c94bee86de83d85f9e504d594549fce483cfd
Ivan Spirikov <35290162+k0tikov@users.noreply.github.com>
2019-05-08 16:38:57 +0300

    item behaviour

---
+++ New Unity Project/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+	public override void Interact()
+	{
+		base.Interact();
+		PickUp();
+	}
+	
+	
+	void PickUp()
+	{
+		Debug.Log("picking up");
+		//add item to inventory
+		Destroy(gameObject);
+	}
+}
