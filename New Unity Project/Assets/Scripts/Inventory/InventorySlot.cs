e71601d59b7bd1b111ea6416eb249e0bedfb62ba
Ivan <ivanspirit6@gmail.com>
2019-05-14 23:34:39 +0300

    inventory ui

---
+++ New Unity Project/Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+	public Image icon;
+	
+	public Item item;
+	
+	public void AddItem(Item newItem)
+	{
+		item = newItem;
+		
+		icon.sprite = item.icon;
+		icon.enabled = true;
+	}
+	
+	public void ClearSlot()
+	{
+		item = null;
+		
+		icon.sprite = null;
+		enabled = false;
+	}
+}
